5fd48e557393b6d3580d45a3ae2d56b8869b6f91
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-05-15 12:20:56 -0500

    script.js outline

--- working-folder/script.js
+++ working-folder/script.js
@@ -4,6 +4,31 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatepassword(){
+    console.log("The Generate Password button has been clicked")
+    // promt the user for the password criteria
+
+    // Password length between 8, 128
+    var randomNumber = prompt("select a password length between 8 and 128 characters");    
+    function randomNumber(number){
+        
+        var number = prompt("enter length")
+        function numberLength(number){
+            if(isNaN(number))
+                return("Please enter a valid number");
+        }
+}
+    // numberic, lowercase, uppercase, or special characters
+    // validate user input
+    
+    
+
+    // display password on the page
+    return "Generated Password will go here"
+}
+
+
+
 // Write a password to the #password input
 function writePassword(){
     var password = generatepassword();
