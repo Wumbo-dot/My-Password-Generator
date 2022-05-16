7b28f949340a6cf5ddd6a23d463c377ddde98155
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-05-15 20:19:59 -0500

    added comments and user prompts incase cancel is selected

--- working-folder/script.js
+++ working-folder/script.js
@@ -12,12 +12,12 @@ function randomInt(min, max){
 }
 // get random variable
 
-function getRandomItem(list){
+function randomOption(list){
     return list[randomInt(0, list.length)]
 }
 
 
-
+//start of the application
 function generatepassword(){
     var userInput = window.prompt("What is your desired password length?")
 
@@ -25,18 +25,20 @@ function generatepassword(){
 
     if(isNaN(passwordLength)){
         window.alert("That is not a number")
+        return("please try again")
     }else{
         window.alert("You entered "+ passwordLength)
     }
     if(passwordLength < 8 || passwordLength > 128){
         window.alert("Password must be betweeen 8 and 128 characters long")
+        window.alert("Please try again")
         return
     }
-
-    var userWantsNumbers = window.confirm("would you like to include numbers in your password? ")
-    var userWantsSymbols  = window.confirm("would you like to include Special characters in your password? ")
-    var userWantsLowercase = window.confirm("would you like to include lower case letters in your password? ")
-    var userWantsUppercase = window.confirm("would you like to include uppercase letters in your password? ")
+// questions that users are prompted to answer
+    var enteredNumberList = window.confirm("would you like to include Numbers in your password? ")
+    var enteredSpecialList  = window.confirm("would you like to include Special Characters in your password? ")
+    var enteredlower = window.confirm("would you like to include Lowercase Letters in your password? ")
+    var enteredUpperList = window.confirm("would you like to include Uppercase Letters in your password? ")
 
     var numberList = ["1","2","3","4","5","6","7","8","9","0"]
     var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
@@ -46,19 +48,19 @@ function generatepassword(){
     var optionsCart = []
 
 
-    if (userWantsNumbers === true){
+    if (enteredNumberList === true){
         optionsCart.push(numberList)
     }
-    if (userWantsSymbols === true){
+    if (enteredSpecialList === true){
         optionsCart.push(symbolList)
     }
-    if (userWantsLowercase === true){
+    if (enteredlower === true){
         optionsCart.push(lowercaseList)
     }
-    if (userWantsUppercase === true) {
+    if (enteredUpperList === true) {
         optionsCart.push(uppercaseList)
     }
-
+//  if the user clicks cancel on all options
     if(optionsCart.length === 0){
         optionsCart.push(uppercaseList)
     }
@@ -66,8 +68,8 @@ function generatepassword(){
     var generatedPassword = ""
 
     for(var i = 0; i < passwordLength; i++){
-        var randomList = getRandomItem(optionsCart)
-        var randomChar = getRandomItem(randomList)
+        var randomList = randomOption(optionsCart)
+        var randomChar = randomOption(randomList)
         generatedPassword += randomChar
     }
 
